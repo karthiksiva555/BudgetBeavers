b34911a73a6bdc7720eb46e2151c688be9b51970
Siva Narisetty <sivanarisetty@geotab.com>
2025-07-22 11:03:46 -0400

    Add unit tests for UpdateAsync method of HomeService.

--- src/BudgetBeavers.API/BudgetBeavers.Application.Tests/Services/HomeServiceTests.cs
+++ src/BudgetBeavers.API/BudgetBeavers.Application.Tests/Services/HomeServiceTests.cs
@@ -1,6 +1,7 @@
 using AutoFixture;
 using BudgetBeavers.Application.Dtos.HomeDtos;
 using BudgetBeavers.Application.Services;
+using BudgetBeavers.Core.Entities;
 using BudgetBeavers.Core.Interfaces;
 using FluentAssertions;
 using Moq;
@@ -53,7 +54,7 @@ public class HomeServiceTests : TestBase
         var home = createHomeDto.ToEntity();
         var expectedHomeDto = home.ToDto();
 
-        _homeRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Core.Entities.Home>()))
+        _homeRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Home>()))
             .ReturnsAsync(home);
 
         // Act
@@ -61,7 +62,90 @@ public class HomeServiceTests : TestBase
 
         // Assert
         result.Should().BeEquivalentTo(expectedHomeDto);
-        _homeRepositoryMock.Verify(repo => repo.AddAsync(It.IsAny<Core.Entities.Home>()), Times.Once);
+        _homeRepositoryMock.Verify(repo => repo.AddAsync(It.IsAny<Home>()), Times.Once);
+    }
+    
+    #endregion
+    
+    #region UpdateAsync
+    
+    [Fact]
+    public async Task UpdateAsync_UpdateHomeDtoIsNull_ThrowsArgumentNullExceptionAsync()
+    {
+        Func<Task> act = async () => await _homeService.UpdateAsync(Guid.NewGuid(), null!);
+        
+        await act.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("updateHomeDto");
+    }
+    
+    [Fact]
+    public async Task UpdateAsync_NameIsInvalid_ThrowsArgumentExceptionAsync()
+    {
+        var updateHomeDto = Fixture.Build<UpdateHomeDto>()
+            .With(h => h.Name, (string?)null)
+            .Create();
+        
+        Func<Task> act = async () => await _homeService.UpdateAsync(Guid.NewGuid(), updateHomeDto);
+        
+        await act.Should().ThrowAsync<ArgumentException>()
+            .WithParameterName(nameof(updateHomeDto.Name))
+            .Where(e => e.Message.Contains("cannot be null or whitespace."));
+    }
+    
+    [Fact]
+    public async Task UpdateAsync_HomeIdIsInvalid_ThrowsArgumentExceptionAsync()
+    {
+        var updateHomeDto = Fixture.Build<UpdateHomeDto>()
+            .With(h => h.Name, "Updated Home")
+            .Create();
+        
+        Func<Task> act = async () => await _homeService.UpdateAsync(Guid.Empty, updateHomeDto);
+        
+        await act.Should().ThrowAsync<ArgumentException>()
+            .WithParameterName("id")
+            .Where(e => e.Message.Contains("cannot be empty."));
+    }
+    
+    [Fact]
+    public async Task UpdateAsync_HomeDoesNotExist_ThrowsKeyNotFoundExceptionAsync()
+    {
+        var updateHomeDto = Fixture.Build<UpdateHomeDto>().With(h => h.Name, "Updated Home").Create();
+        var homeId = Guid.NewGuid();
+
+        _homeRepositoryMock.Setup(repo => repo.GetByIdAsync(homeId))
+            .ReturnsAsync((Home?)null);
+
+        Func<Task> act = async () => await _homeService.UpdateAsync(homeId, updateHomeDto);
+        
+        await act.Should().ThrowAsync<KeyNotFoundException>()
+            .WithMessage($"No entity found with the provided id: {homeId}.");
+    }
+    
+    [Fact]
+    public async Task UpdateAsync_ValidHomeIdAndDto_UpdatesHomeAndReturnsDtoAsync()
+    {
+        // Arrange
+        var createdAt = DateTime.UtcNow.AddDays(-10);
+        var homeId = Guid.NewGuid();
+        var updateHomeDto = Fixture.Build<UpdateHomeDto>()
+            .With(h => h.Name, "Updated Home")
+            .Create();
+        
+        var existingHome = new Home { Id = homeId, Name = "Old Home", CreatedAt = createdAt };
+        var updatedHome = new Home { Id = homeId, Name = updateHomeDto.Name, CreatedAt = createdAt };
+        var expectedHomeDto = updatedHome.ToDto();
+
+        _homeRepositoryMock.Setup(repo => repo.GetByIdAsync(homeId))
+            .ReturnsAsync(existingHome);
+        _homeRepositoryMock.Setup(repo => repo.UpdateAsync(It.IsAny<Home>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _homeService.UpdateAsync(homeId, updateHomeDto);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedHomeDto);
+        _homeRepositoryMock.Verify(repo => repo.UpdateAsync(It.IsAny<Home>()), Times.Once);
     }
     
     #endregion
