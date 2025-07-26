002d16a3886b5408d66c82df8f448bff9e895518
Siva Karthik Narisetty <karthik.siva555@gmail.com>
2025-07-26 15:31:44 -0400

    feat: add validations on required fields
    Use Guard class to validate all the required fields of User object

--- src/BudgetBeavers.API/BudgetBeavers.Application/Services/UserService.cs
+++ src/BudgetBeavers.API/BudgetBeavers.Application/Services/UserService.cs
@@ -1,13 +1,20 @@
 using BudgetBeavers.Application.Dtos.UserDtos;
 using BudgetBeavers.Application.Interfaces;
+using BudgetBeavers.Application.Utilities;
 using BudgetBeavers.Core.Interfaces;
 
 namespace BudgetBeavers.Application.Services;
 
 public class UserService(IUserRepository userRepository): IUserService
 {
-    public Task<UserDto> AddAsync(CreateUserDto createHomeDto)
+    public Task<UserDto> AddAsync(CreateUserDto createUserDto)
     {
+        Guard.AgainstNull(createUserDto, nameof(createUserDto));
+        Guard.AgainstNullOrWhiteSpace(createUserDto.FirstName, nameof(createUserDto.FirstName));
+        Guard.AgainstNullOrWhiteSpace(createUserDto.LastName, nameof(createUserDto.LastName));
+        Guard.AgainstNullOrWhiteSpace(createUserDto.Email, nameof(createUserDto.Email));
+        Guard.AgainstNullOrWhiteSpace(createUserDto.Password, nameof(createUserDto.Password));
+        
         throw new NotImplementedException();
     }
 
